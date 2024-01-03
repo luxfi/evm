7a2a3fee4f3c2bdf29c5bd9be8922462c2dc1e18
Zach Kelling <z@zeekay.io>
2024-01-03 09:52:28 +0100

    Sync upstream

--- precompile/contracts/nativeminter/unpack_pack_test.go
+++ precompile/contracts/nativeminter/unpack_pack_test.go
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 // (c) 2019-2023, Ava Labs, Inc. All rights reserved.
+=======
+// (c) 2019-2023, Lux Partners Limited. All rights reserved.
+>>>>>>> d5328b4 (Sync upstream)
 // See the file LICENSE for licensing terms.
 
 package nativeminter
