e16b6784db9429c2583eb655790f5eb4309560af
Zach Kelling <z@zeekay.io>
2025-07-17 16:54:47 -0500

    fix: remove libevm dependencies
    - Replace ethereum/go-ethereum/libevm with ava-labs/libevm imports
    - Prepare for removal of libevm dependencies

--- triedb/hashdb/database.go
+++ triedb/hashdb/database.go
@@ -37,7 +37,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/libevm/stateconf"
+	"github.com/ava-labs/libevm/stateconf"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/metrics"
 	"github.com/ethereum/go-ethereum/rlp"
