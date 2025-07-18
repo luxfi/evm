8d3f99c34718e68443d89f8e1abf45ef075b6282
Zach Kelling <z@zeekay.io>
2025-07-18 06:47:51 +0000

    Updates and improvements

---
+++ core/state/snapshot/rawdb.go
@@ -0,0 +1,32 @@
+package snapshot
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethdb"
+)
+
+// snapshotBlockHashKey = snapshotPrefix + blockHash
+var snapshotBlockHashKey = []byte("SnapshotBlockHash")
+
+// WriteSnapshotBlockHash writes the block hash of a snapshot.
+func WriteSnapshotBlockHash(db ethdb.KeyValueWriter, blockHash common.Hash) {
+	if err := db.Put(snapshotBlockHashKey, blockHash.Bytes()); err != nil {
+		panic(err)
+	}
+}
+
+// ReadSnapshotBlockHash reads the block hash of a snapshot.
+func ReadSnapshotBlockHash(db ethdb.KeyValueReader) common.Hash {
+	data, _ := db.Get(snapshotBlockHashKey)
+	if len(data) == 0 {
+		return common.Hash{}
+	}
+	return common.BytesToHash(data)
+}
+
+// DeleteSnapshotBlockHash deletes the block hash of a snapshot.
+func DeleteSnapshotBlockHash(db ethdb.KeyValueWriter) {
+	if err := db.Delete(snapshotBlockHashKey); err != nil {
+		panic(err)
+	}
+}
