4048a25b6da30082f17e1accc429acd4c79042d2
Zach Kelling <z@zeekay.io>
2025-08-02 06:10:37 +0000

    Fix more compilation errors: unused imports, DBOverride workaround, validator state compatibility

--- triedb/pathdb/database.go
+++ triedb/pathdb/database.go
@@ -106,11 +106,16 @@ type Config struct {
 }
 
 func (c Config) BackendConstructor(diskdb ethdb.Database) triedb.DBOverride {
-	// Note: This is a workaround for version mismatch
-	// The pathdb.Database doesn't directly implement triedb.Database
-	// This requires proper version alignment between EVM and geth
-	return nil
+	// Note: There's a type mismatch between pathdb.Database and triedb.Database
+	// This is a version compatibility issue that needs to be resolved
+	// For now, we create a dummy implementation
+	return &dummyDBOverride{}
 }
+
+// dummyDBOverride is a temporary workaround for the type mismatch
+type dummyDBOverride struct{}
+
+func (d *dummyDBOverride) Close() error { return nil }
 
 // sanitize checks the provided user configurations and changes anything that's
 // unreasonable or unworkable.
