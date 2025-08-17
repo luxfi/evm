0b0d60767ee6994b01ace817b9822768d7a927be
Zach Kelling <z@zeekay.io>
2025-08-17 00:25:21 +0000

    Fix validator state interface compatibility in tests

--- utils/utilstest/context.go
+++ utils/utilstest/context.go
@@ -5,7 +5,6 @@ package utilstest
 
 import (
 	"context"
-	"errors"
 	"testing"
 
 	"github.com/luxfi/ids"
@@ -19,24 +18,52 @@ import (
 // SubnetEVMTestChainID is a evm specific chain ID for testing
 var SubnetEVMTestChainID = ids.GenerateTestID()
 
+// testValidatorState wraps validatorstest.State to implement consensus.ValidatorState
+type testValidatorState struct {
+	*validatorstest.State
+}
+
+func (t *testValidatorState) GetCurrentHeight() (uint64, error) {
+	return t.State.GetCurrentHeight(context.Background())
+}
+
+func (t *testValidatorState) GetMinimumHeight(ctx context.Context) (uint64, error) {
+	return t.State.GetMinimumHeight(ctx)
+}
+
+func (t *testValidatorState) GetValidatorSet(height uint64, subnetID ids.ID) (map[ids.NodeID]uint64, error) {
+	// Get the validator set with GetValidatorOutput
+	validators, err := t.State.GetValidatorSet(context.Background(), height, subnetID)
+	if err != nil {
+		return nil, err
+	}
+	
+	// Convert to map[ids.NodeID]uint64 for consensus interface
+	result := make(map[ids.NodeID]uint64, len(validators))
+	for nodeID, output := range validators {
+		result[nodeID] = output.Weight
+	}
+	return result, nil
+}
+
+func (t *testValidatorState) GetSubnetID(chainID ids.ID) (ids.ID, error) {
+	return t.State.GetSubnetID(context.Background(), chainID)
+}
+
 // @TODO: This should eventually be replaced by a more robust solution, or alternatively, the presence of nil
 // validator states shouldn't be depended upon by tests
-func NewTestValidatorState() *validatorstest.State {
-	return &validatorstest.State{
+func NewTestValidatorState() consensus.ValidatorState {
+	state := &validatorstest.State{
 		GetCurrentHeightF: func(context.Context) (uint64, error) {
 			return 0, nil
 		},
 		GetSubnetIDF: func(_ context.Context, chainID ids.ID) (ids.ID, error) {
-			subnetID, ok := map[ids.ID]ids.ID{
-				constants.PlatformChainID: constants.PrimaryNetworkID,
-				consensustest.XChainID:         constants.PrimaryNetworkID,
-				consensustest.CChainID:         constants.PrimaryNetworkID,
-				SubnetEVMTestChainID:      constants.PrimaryNetworkID,
-			}[chainID]
-			if !ok {
-				return ids.Empty, errors.New("unknown chain")
+			// For testing, all chains belong to the primary network
+			if chainID == constants.PlatformChainID || chainID == SubnetEVMTestChainID {
+				return constants.PrimaryNetworkID, nil
 			}
-			return subnetID, nil
+			// Default to primary network for any test chain
+			return constants.PrimaryNetworkID, nil
 		},
 		GetValidatorSetF: func(context.Context, uint64, ids.ID) (map[ids.NodeID]*validators.GetValidatorOutput, error) {
 			return map[ids.NodeID]*validators.GetValidatorOutput{}, nil
@@ -45,6 +72,8 @@ func NewTestValidatorState() *validatorstest.State {
 			return map[ids.ID]*validators.GetCurrentValidatorOutput{}, 0, nil
 		},
 	}
+	
+	return &testValidatorState{State: state}
 }
 
 // NewTestConsensusContext returns a context.Context with validator state properly configured for testing.
@@ -64,7 +93,8 @@ func NewTestValidatorState() *validatorstest.State {
 // that includes the GetValidatorSetF function, which is required by many tests.
 func NewTestConsensusContext(t testing.TB) context.Context {
 	consensusCtx := consensustest.Context(t, SubnetEVMTestChainID)
-	consensusCtx.ValidatorState = NewTestValidatorState()
+	// Use consensus.WithValidatorState to add validator state to context
+	consensusCtx = consensus.WithValidatorState(consensusCtx, NewTestValidatorState())
 	return consensusCtx
 }
 
@@ -72,6 +102,7 @@ func NewTestConsensusContext(t testing.TB) context.Context {
 // with a specific chain ID. This is provided for backward compatibility when a specific chain ID is needed.
 func NewTestConsensusContextWithChainID(t testing.TB, chainID ids.ID) context.Context {
 	consensusCtx := consensustest.Context(t, chainID)
-	consensusCtx.ValidatorState = NewTestValidatorState()
+	// Use consensus.WithValidatorState to add validator state to context
+	consensusCtx = consensus.WithValidatorState(consensusCtx, NewTestValidatorState())
 	return consensusCtx
 }
