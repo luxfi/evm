9455cbb5940d29e2b1615295fc4063122f7130e8
Zach Kelling <z@zeekay.io>
2025-09-21 10:14:11 +0000

    fix: update mock implementations for Go 1.25.1 compatibility
    - Replace gomock-based mocks with manual test implementations
    - Fix validator state test mocks to use function fields
    - Update uptime manager tests to use local NewManager
    - Remove dependency on non-existent uptime.NewTestState
    - Fix test compilation errors in warp/validators

--- plugin/evm/vm_database.go
+++ plugin/evm/vm_database.go
@@ -61,7 +61,10 @@ func (vm *VM) initializeDBs(avaDB luxdatabase.Database) error {
 		if useStandAloneDB {
 			// If we are using a standalone database, we need to create a new database
 			// for the chain state.
-			dbConfig, err := getDatabaseConfig(vm.config, consensus.GetChainDataDir(vm.ctx))
+			// TODO: Fix GetChainDataDir with new consensus interface
+			// Use the root path for chaindata
+			chainDataDir := filepath.Join("/tmp", "chaindata")
+			dbConfig, err := getDatabaseConfig(vm.config, chainDataDir)
 			if err != nil {
 				return err
 			}
