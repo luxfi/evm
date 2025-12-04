755ace1cd62fb74f2442fa4a4b1d807ec7b731ff
Zach Kelling <z@zeekay.io>
2025-12-04 15:06:56 -0800

    Remove node dependency from EVM core
    - Copied api and utils/rpc packages from node to remove dependency
    - Updated imports to use local packages
    - Removed node from go.mod requirements
    - Core EVM package now builds without node dependency
    - Only runner/main (for testing) still uses node/vms/rpcchainvm
    
    This allows EVM to be used as a library without pulling in the entire node package tree.

--- plugin/evm/config/config.go
+++ plugin/evm/config/config.go
@@ -237,7 +237,6 @@ type Config struct {
 	// WarpOffChainMessages encodes off-chain messages (unrelated to any on-chain event ie. block or AddressedCall)
 	// that the node should be willing to sign.
 	// Note: only supports AddressedCall payloads as defined here:
-	// https://github.com/luxfi/node/tree/7623ffd4be915a5185c9ed5e11fa9be15a6e1f00/vms/platformvm/warp/payload#addressedcall
 	WarpOffChainMessages []hexutil.Bytes `json:"warp-off-chain-messages"`
 
 	// RPC settings
