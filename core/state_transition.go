dd93df8850098cde94539fdfc3532deaf90066c5
Zach Kelling <z@zeekay.io>
2025-12-11 04:57:36 +0000

    refactor: use luxfi/crypto/kzg4844 instead of geth/crypto/kzg4844
    Use externalized kzg4844 from luxfi/crypto package for consistent
    crypto usage across all luxfi packages.
    
    - Update geth to v1.16.43 (which also uses luxfi/crypto/kzg4844)
    - Update crypto to v1.17.15
    - Remove geth/crypto/kzg4844 from allowed packages

--- core/state_transition.go
+++ core/state_transition.go
@@ -42,7 +42,7 @@ import (
 	"github.com/luxfi/geth/core/tracing"
 	"github.com/luxfi/geth/core/types"
 	"github.com/luxfi/geth/core/vm"
-	"github.com/luxfi/geth/crypto/kzg4844"
+	"github.com/luxfi/crypto/kzg4844"
 	ethparams "github.com/luxfi/geth/params"
 )
 
