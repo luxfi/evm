62b7ba734957ce4fc0345693c7943f342c17ccf0
Zach Kelling <z@zeekay.ai>
2025-12-21 03:49:13 -0800

    Fix build, remove evm-node

--- plugin/main.go
+++ plugin/main.go
@@ -5,8 +5,6 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"time"
 
 	"github.com/luxfi/evm/plugin/evm"
 	"github.com/luxfi/evm/plugin/runner"
@@ -14,11 +12,6 @@ import (
 )
 
 func main() {
-	// DEBUG: Write to file to verify plugin is starting
-	if f, err := os.OpenFile("/tmp/evm_plugin_start.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644); err == nil {
-		fmt.Fprintf(f, "[%s] EVM plugin main() started, PID=%d\n", time.Now().Format(time.RFC3339), os.Getpid())
-		f.Close()
-	}
 	versionString := fmt.Sprintf("Lux-EVM/%s [node=%s, rpcchainvm=%d]", evm.Version, version.Current, version.RPCChainVMProtocol)
 	runner.Run(versionString)
 }
