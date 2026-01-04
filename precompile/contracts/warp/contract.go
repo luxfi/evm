c359103920290d9f3a12c926a27904fcc16af10d
Zach Kelling <z@zeekay.ai>
2026-01-03 17:37:03 -0800

    Use luxfi/math/math instead of geth/common/math

--- precompile/contracts/warp/contract.go
+++ precompile/contracts/warp/contract.go
@@ -12,7 +12,7 @@ import (
 	"github.com/luxfi/evm/accounts/abi"
 	"github.com/luxfi/evm/precompile/contract"
 	"github.com/luxfi/geth/common"
-	"github.com/luxfi/geth/common/math"
+	"github.com/luxfi/math/math"
 	"github.com/luxfi/geth/core/types"
 	"github.com/luxfi/geth/core/vm"
 	"github.com/luxfi/warp"
